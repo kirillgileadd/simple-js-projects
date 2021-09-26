5675a1f51add9ad0350092547c6a9192b0c2622c
kirillgileadd <kirillgilead@gmail.com>
2021-09-26 11:10:46 +0300

    commit

---
+++ 04-board/script.js
@@ -0,0 +1,37 @@
+const board = document.querySelector('#board')
+const colors = ['red', 'pink', 'blue', 'purple', 'gold']
+const SQERE_ITEMS = 462;
+
+for (let i = 0; i < SQERE_ITEMS; i++) {
+   const squere = document.createElement('div')
+   squere.classList.add('squere')
+
+   squere.addEventListener('mouseover', () => {
+      setColor(squere)
+   })
+
+   squere.addEventListener('mouseleave', () => {
+      setTimeout(() => {
+         setTimeout(deleteColor(squere))
+      }, 5000)
+   })
+
+
+   board.append(squere)
+}
+
+const setColor = (e) => {
+   const color = getRandomColors(colors)
+   e.style.backgroundColor = color
+   e.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+}
+
+const deleteColor = (e) => {
+   e.style.backgroundColor = '#1d1d1d'
+   e.style.boxShadow = '0 0 2px black'
+}
+
+const getRandomColors = (colors) => {
+   const index = Math.floor(Math.random() * colors.length)
+   return colors[index]
+}
